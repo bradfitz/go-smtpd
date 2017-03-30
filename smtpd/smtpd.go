8ebf70ba0acf0674302879b9872da16f478c29d5
Brad Fitzpatrick <brad@danga.com>
2017-03-30 14:04:59 -0700

    Remove ancient Makefile, add copyright header.

--- smtpd/smtpd.go
+++ smtpd/smtpd.go
@@ -1,3 +1,7 @@
+// Copyright 2011 The go-smtpd Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package smtpd implements an SMTP server. Hooks are provided to customize
 // its behavior.
 package smtpd
